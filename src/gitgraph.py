df235018a25e392e07f491fa90d3a41386aa6084
Nathan BLONDEL <slowerzs@hackademint.org>
2021-04-11 10:48:53 -0400

    Correction de l'utilisation multi branches

--- src/gitgraph.py
+++ src/gitgraph.py
@@ -5,45 +5,73 @@ import sys
 
 
 class GitGraph:
-	def __init__(self, paths):
-		self.repos = []
-		for path in paths:
-			try:
-				self.repos.append(Repo(path))
-			except InvalidGitRepositoryError:
-				print("Un des dossiers proposé n'est pas un dépot git.")
-				sys.exit(1)
-
-	def getBranches(self) -> Set[Head]:
-	    branches = []
-	    for repo in self.repos :
-	        branches += repo.branches
-	    return set(branches)
-	    
-	def getRemotes(self, repo: Repo) -> Set[Remote]:
-	    remotes = []
-	    for repo in self.repos :
-	        remotes += repo.remotes
-	    return set(remotes)
-	    
-	def createSubGraphRemote(self, branch: Remote) -> Digraph:
-            graph = Digraph()
-            repo = branch.repo
-
-            for ref in repo.iter_commits(branch):
-                graph.node(ref.hexsha, label=ref.message)
-                for parent in ref.parents:
-                    graph.edge(ref.hexsha, parent.hexsha)	
-
-            return graph
-
-	def createSubGraph(self, branch: Head) -> Digraph:
-            graph = Digraph()
-            repo = branch.repo
-
-            for ref in repo.iter_commits(branch):
-                    graph.node(ref.hexsha, label=ref.message)
-                    for parent in ref.parents:
-                            graph.edge(ref.hexsha, parent.hexsha)	
+    def __init__(self, paths):
+        self.repos = []
+        self.branches = []
+        self.remotes = []
             
-            return graph
+        for path in paths:
+            try:
+                self.repos.append(Repo(path))
+            except InvalidGitRepositoryError:
+                print("Un des dossiers proposé n'est pas un dépot git.")
+                sys.exit(1)
+        
+        self.branches += self.getBranches()
+        self.remotes += self.getRemotes()
+        
+
+    def render(self) -> None:
+        self.generateFinalGraph(self.branches, self.remotes).render("/output/output_dot")
+
+
+    def getBranches(self) -> List[Head]:
+        branches = []
+        for repo in self.repos :
+            branches += repo.branches
+        return branches
+        
+    def getRemotes(self) -> List[Remote]:
+        remotes = []
+        for repo in self.repos :
+            remotes += repo.remotes
+        return list(set(remotes))
+        
+    def createSubGraphRemote(self, branch: Remote, prefix: str) -> Digraph:
+        graph = Digraph()
+        repo = branch.repo
+
+        for ref in repo.iter_commits(branch):
+            graph.node(prefix + ref.hexsha, label=ref.message)
+            for parent in ref.parents:
+                graph.edge(prefix + ref.hexsha, prefix + parent.hexsha)	
+
+        return graph
+
+    def createSubGraph(self, branch: Head, prefix: str) -> Digraph:
+        graph = Digraph()
+        repo = branch.repo
+        for ref in repo.iter_commits(branch):
+                graph.node(prefix + ref.hexsha, label=ref.message)
+                for parent in ref.parents:
+                        graph.edge(prefix + ref.hexsha, prefix + parent.hexsha)	
+        
+        return graph
+
+
+    def generateFinalGraph(self, branches: List[Head], remotes: List[Remote]) -> Digraph:
+        graph = Digraph()
+        i = 0
+        for branch in branches:
+            temp_graph = self.createSubGraph(branch, str(i) + "_")
+            graph.subgraph(temp_graph)
+            i += 1
+
+        for remote in remotes:
+            temp_graph = self.createSubGraphRemote(remote, str(i) + "_")
+            graph.subgraph(temp_graph)
+            i += 1
+
+        return graph
+
+
--- src/main.py
+++ src/main.py
@@ -4,10 +4,4 @@ from graphviz import Digraph
 
 if __name__ == '__main__':
     ggraph = gitgraph.GitGraph([path.join("/sources", i) for i in listdir("/sources")])
-    b = ggraph.getBranches()
-    final_graph = Digraph()
-    for br in b:
-        temp_graph = ggraph.createSubGraph(br)
-        final_graph.subgraph(temp_graph)
-
-    final_graph.render("/output/output_dot")
+    ggraph.render()
